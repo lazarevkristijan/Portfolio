4cb0cecedcd497cb750aa792696e876f4c2fc683
Kristijan Lazarev <133780727+lazarevkristijan@users.noreply.github.com>
2023-08-04 11:47:34 +0200

    imgUrl typo

--- constants/index.js
+++ constants/index.js
@@ -51,7 +51,7 @@ export const projects = [
     name: 'calculator',
     description: 'calc app with dark mode',
     tools: 'html / css / js',
-    imgUrl: '/projectImages/calculatorProjectImage.jpg',
+    imgUrl: '/projectImages/calculatorProjectImage.png',
     liveUrl: 'https://calculator-app-mq64.onrender.com/',
     githubUrl: 'https://github.com/lazarevkristijan/Calculator-App',
   },
